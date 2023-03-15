1b8c4cedcc148894fd3ec7328e1f1e49a13d6c05
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2023-03-15 12:35:36 +0100

    better organization of test read files

--- tests/testthat/test_readFile.R
+++ tests/testthat/test_readFile.R
@@ -1,4 +1,4 @@
-test_that("reads and formats ActiGraph agd data", {
+test_that("reads and formats data correctly", {
 
   # function to check if is ISO8601
   # https://github.com/wadpac/GGIR/blob/master/R/is.ISO8601.R
@@ -14,20 +14,28 @@ test_that("reads and formats ActiGraph agd data", {
     return(is.ISO)
   }
 
-  # files
-  paths = dir(system.file("testfiles_agd/", package = "stepmetrics"), full.names = TRUE)
-
   # tests ------
-  # epoch = 60 seconds
+  # fitbit files -----
+  paths = dir(system.file("testfiles_fitbit/", package = "stepmetrics"), full.names = TRUE)
+  data = readFile(paths)
+
+  expect_equal(dim(data), c(1440*3, 2))
+  expect_equal(colnames(data), c("timestamp", "steps"))
+  expect_true(is.ISO8601(data$timestamp[1]))
+
+  # actigraph agd files -----
+  paths = dir(system.file("testfiles_agd/", package = "stepmetrics"), full.names = TRUE)
   file1 = grep("1min", paths, value = TRUE)
+  file2 = grep("10secs", paths, value = TRUE)
+
+  # 1min epoch
   data = readFile(file1)
 
   expect_equal(dim(data), c(1440*5, 2)) # the sample agd file contains 5 complete days
   expect_equal(colnames(data), c("timestamp", "steps"))
   expect_true(is.ISO8601(data$timestamp[1]))
 
-  # epoch = 10 seconds
-  file2 = grep("10secs", paths, value = TRUE)
+  # 10 sec epoch
   data = readFile(file2)
 
   expect_equal(dim(data), c(15232, 2))
