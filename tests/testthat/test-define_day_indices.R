a3111c803bfe42602cff0ad470b917568a7b7f19
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2023-03-15 13:12:28 +0100

    remove GGIR from test define day indices

--- tests/testthat/test-define_day_indices.R
+++ tests/testthat/test-define_day_indices.R
@@ -1,9 +1,15 @@
 test_that("creates vector for days", {
 
+  chartime2iso8601 = function(x, tz) {
+    POStime = as.POSIXlt(as.numeric(as.POSIXlt(x, tz)), origin = "1970-1-1", tz)
+    POStimeISO = strftime(POStime, format = "%Y-%m-%dT%H:%M:%S%z")
+    return(POStimeISO)
+  }
+
   # dummy timestamp vector to test -----
   ts = seq.POSIXt(from = as.POSIXlt("2023-01-01 00:00:00"),
                   to = as.POSIXlt("2023-01-04 23:59:00"), by = 60)
-  ts = GGIR::POSIXtime2iso8601(ts, tz = "")
+  ts = chartime2iso8601(format(ts),tz = "")
 
   # tests -----
   day = define_day_indices(ts)
