126d6b953164636b8bd811a6e0a114a394c1efef
Jairo H Migueles <jairo.hidalgo.migueles@gmail.com>
2023-05-22 16:05:21 +0200

    handle when only 1 midnight

--- R/define_day_indices.R
+++ R/define_day_indices.R
@@ -12,8 +12,8 @@
 #' }
 define_day_indices = function(ts) {
   mnightsi = grep("00:00:00", ts, fixed = T)
-  if (length(mnightsi) > 0) {
-    day = rep(NA, times = length(ts))
+  day = rep(NA, times = length(ts))
+  if (length(mnightsi) > 1) { # loop through midnights if more than 1
     d = 1
     # Loop through the rest of the days
     for (j in 1:length(mnightsi)) {
@@ -21,7 +21,7 @@ define_day_indices = function(ts) {
         if (j == 1 & mnightsi[j] > 1) {
           day[1:mnightsi[j]] = d
         } else if (j == 1 & mnightsi[j] == 1) {
-          next()
+          next
         } else {
           day[mnightsi[j - 1]:mnightsi[j]] = d
         }
@@ -31,6 +31,9 @@ define_day_indices = function(ts) {
         day[mnightsi[j]:length(day)] = d
       }
     }
+  } else if (length(mnightsi) == 1) { # if only 1 midnight
+    day[1:(mnightsi - 1)] = 1
+    day[mnightsi:length(ts)] = 2
   } else { # less of 1 day of data
    day = rep(1, length(ts))
   }
